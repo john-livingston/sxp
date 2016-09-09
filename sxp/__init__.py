5c95ee429debcd3f6c312c31fae69c2f2657a3ae
john-livingston <jliv84@gmail.com>
2016-09-09 17:14:12 +0900

    added dataset class and pixel util function for temporary use until format upgrade

--- sxp/__init__.py
+++ sxp/__init__.py
@@ -1,3 +1,45 @@
 import phot
 import util
 import vis
+
+
+class SxpDataset(dict):
+
+    """
+    temporarily here until the output gets upgraded from current (bad) format.
+    requires the user to supply a YAML config file with various config. values,
+    see examples dir.
+    """
+
+    def __init__(self, setup, aor=None, geom='3x3', radius='setup'):
+
+        self._config = setup['config']
+        self['star'] = self._config['star']
+        self['planet'] = self._config['planet']
+        if aor:
+            self['aor'] = aor
+        else:
+            self['aor'] = self._config['aor']
+        if geom:
+            self['geom'] = geom
+        else:
+            self['geom'] = self._config['geom']
+
+        cube, t, f, r, s, c = load_data(self._config['data_dir'], self['aor'])
+
+        self['pix'] = util.get_pix(cube, geom=self['geom'], normalize=True)
+        self['t'] = t
+        if radius == 'setup':
+            i = r.index(self._config['radius'])
+        elif radius == 'auto':
+            i = select_radius(f)
+        elif type(radius) == float:
+            try:
+                i = r.index(radius)
+            except:
+                sys.exit('invalid radius selection')
+        print("using radius: {}".format(r[i]))
+        self['radius'] = r[i]
+        self['f'] = f[i]
+        self['fcor'] = f[i].copy() # initially same as f, but can be updated
+        self['s'] = s[i]
--- sxp/util.py
+++ sxp/util.py
@@ -31,3 +31,41 @@ def mkdir(d):
 def mkdirs(*args):
     for d in args:
         mkdir(d)
+
+
+def get_pix(cube, cen=None, geom='3x3', normalize=False):
+
+    """
+    assumes the cube has shape (n,k,k), where n is the number of frames and k
+    is the width of each (square) frame, and that k > 5.
+    """
+
+    def fix_nan(im):
+        im[np.isnan(im)] = np.median(im[~np.isnan(im)])
+
+    if not cen:
+        med_im = np.median(cube, axis=0)
+        fix_nan(med_im)
+        cx, cy = centroid_com(med_im)
+        cx, cy = map(int, map(round, [cx, cy]))
+        print "centroid: {}, {}".format(cx, cy)
+    else:
+        cx, cy = map(int, cen)
+    if geom == '3x3':
+        i = 1
+    elif geom == '5x5':
+        i = 2
+    else:
+        raise ValueError("geometry not supported")
+
+    x0 = cx - i
+    x1 = cx + i + 1
+    y0 = cy - i
+    y1 = cy + i + 1
+    sub_cube = cube[:, x0:x1, y0:y1]
+    pixels = sub_cube.reshape(cube.shape[0], sub_cube.shape[1]**2)
+
+    if normalize:
+        for i in pixels: i /= i.sum()
+        
+    return pixels
