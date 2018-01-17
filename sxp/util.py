8b5f0cf0b3b713e5b75ba28abb24ea225e8faf1d
John Livingston <jliv84@gmail.com>
2018-01-17 11:44:09 +0900

    bugfix

--- sxp/util.py
+++ sxp/util.py
@@ -158,6 +158,8 @@ def beta(flux, timestep, start_min=5, stop_min=20):
     betas = []
     for bs in range(min_bs, max_bs + 1):
         nbins = int(round(ndata / bs))
+        if nbins == 0:
+            continue
         sigmaN_theory = sigma1 / np.sqrt(bs) * np.sqrt( nbins / (nbins - 1) )
         sigmaN_actual = np.std(binned(flux, bs))
         beta = sigmaN_actual / sigmaN_theory
